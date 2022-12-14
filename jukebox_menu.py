fba557cbf66fcadfa7fc69d2c02f1268db889b77
Oskar <jantaoskar@gmail.com>
2022-12-15 00:19:08 +0100

    2022_12_15_01

--- jukebox_menu.py
+++ jukebox_menu.py
@@ -1,6 +1,7 @@
 from nested_data import albums
 
 SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
 
 while True:
     print("Please choose your album (invalid choice exits):")
@@ -8,11 +9,18 @@ while True:
         print("{}: {}".format(index + 1, title))
 
     choice = int(input())
-    if 1<= choice <= len(albums):
+    if 1 <= choice <= len(albums):
         songs_list = albums[choice - 1][SONGS_LIST_INDEX]
     else:
         break
 
-    print(albums[choice - 1])
-    print(songs_list)
-    print()
+    print("Please choose your song:")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index + 1, song))
+
+    song_choice = int(input())
+    if 1 <= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+        print("Playing {}".format(title))
+
+    print("=" * 40)
